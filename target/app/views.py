1144e7e821d7d8d12a6ee39c94535f4b3973be03
rednafi <redowan.nafi@gmail.com>
2021-07-15 08:13:10 +0600

    The template is fully decoupled now

--- target/app/views.py
+++ target/app/views.py
@@ -1,3 +1,10 @@
+from django.core.cache import cache
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class PersonView(View):
+    def get(self, request):
+        context = cache.get("09d4a324-7bb3-44f4-b989-902fdb0a11dc")
+        print(context)
+        return render(request, "index.html", context)
--- target/target/urls.py
+++ target/target/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from app.views import PersonView
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("persons/", PersonView.as_view()),
 ]
