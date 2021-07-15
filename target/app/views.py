746dd567fb68f9bc7d733c02b78368ae7d24d115
rednafi <redowan.nafi@gmail.com>
2021-07-15 08:22:40 +0600

    Caching is working spectacularly

--- target/app/views.py
+++ target/app/views.py
@@ -1,3 +1,4 @@
+import httpx
 from django.core.cache import cache
 from django.shortcuts import render
 from django.views import View
@@ -5,6 +6,10 @@ from django.views import View
 
 class PersonView(View):
     def get(self, request):
-        context = cache.get("09d4a324-7bb3-44f4-b989-902fdb0a11dc")
-        print(context)
+        with httpx.Client(http2=True) as session:
+            res = session.get("http://localhost:4000/api/v1/persons_context")
+            if res.status_code == 200:
+                key = res.json()[0]["key"]
+
+        context = cache.get(key)
         return render(request, "index.html", context)
